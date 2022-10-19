359bd988ce6f536d596eb6241dc2923640390591
Pawel Szulczewski <pawel.szulczewski@gmail.com>
2022-10-19 17:00:35 +0200

    Pimped out the output.

--- cliInterface.py
+++ cliInterface.py
@@ -1,11 +1,22 @@
 from conf.constants import CLIENT_VERSION
 from click import clear
 from connection import jellyConnect
-from jellyfin_apiclient_python import api
 from tabulate import tabulate
 
 import cmd
 import sys
+
+def printPrettyTable(data, headers):
+    values = list()
+    fixedHeaders = ["Lp"]
+    print()
+    for i in range(len(data)):
+        val = []
+        val.append(i+1)
+        val.append(data[i])
+        values.append(val)
+    print(tabulate(values, fixedHeaders + list(headers)))
+    print()
 
 class cliInterface(cmd.Cmd):
 
@@ -14,11 +25,17 @@ class cliInterface(cmd.Cmd):
 
     connection = None
 
+    def emptyline(self):
+        self.default('')
+
+    def default(self, arg):
+        print ("\nWhat? Dunno understand ya, type help or ? to list commands.\n")
+
     def do_connect(self, arg):
         'Tries to connect to the server...'
         self.connection = jellyConnect()
         self.client = self.connection.client
-
+        
     def do_quit(self, arg):
         'Says bye-bye to the server'
         print('\nSee you later aligator!\n')
@@ -26,30 +43,29 @@ class cliInterface(cmd.Cmd):
 
     def do_version(self, arg):
         'Prints version of the client'
-        print(CLIENT_VERSION)
+        print("\n{}\n".format(CLIENT_VERSION))
 
     def do_showUsers(self, arg):
         'Shows all registered users'
         if self.connection is None:
             print('Client is not connected! Please connect to the server.')
             return
+
         users = self.client.jellyfin.get_users()
+        usersList = []
         for i in range(len(users)):
-            userName = users[i]["Name"]
-            print(userName)
+            usersList.append(users[i]["Name"])
+
+        printPrettyTable(usersList, ["username"])
 
     def do_recentlyAddedStuff(self, arg):
         'Shows recently added stuff'
         added = self.client.jellyfin.get_recently_added()
-        values = list()
-        print()
+        addedList = []
         for i in range(len(added)):
-            val = [] 
-            val.append(i+1)
-            val.append(added[i]["Name"])
-            values.append(val)
-        print(tabulate(values, headers=["Lp", "Title"]))
-        print()
+            addedList.append(added[i]["Name"])
+
+        printPrettyTable(addedList, ["Name"])
 
     def do_clear(self, arg):
         'Clears the screen'
